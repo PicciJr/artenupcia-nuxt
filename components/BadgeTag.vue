e59a4139841659417665c07b7c38f01c031a4c0d
Andres Piccininno Zugasti <piccijr@MacBook-Pro.home>
2020-06-18 07:13:53 +0200

    Probando purge de tailwind para build

--- components/BadgeTag.vue
+++ components/BadgeTag.vue
@@ -2,7 +2,7 @@
   <div>
     <span
       class="inline-flex items-center px-2 py-1 rounded-md text-sm leading-5 cursor-pointer"
-      :class="setColorStlye"
+      :class="setBadgeStyle"
       @click="setBadgeStatus"
     >
       Badge
@@ -13,34 +13,31 @@
 <script>
 export default {
   props: {
-    color: ''
-  },
-  data() {
-    return {
-      isActive: false
+    /** blue, red, gray */
+    color: {
+      type: String,
+      default: 'blue'
+    },
+    isActive: {
+      type: Boolean,
+      default: false
     }
   },
   computed: {
     /** Setear el estilo del color del badge segun lo que haga el usuario */
-    setColorStlye() {
-      if (!this.isActive)
-        return (
-          'font-medium bg-' +
-          this.color +
-          '-100 text-' +
-          this.color +
-          '-800 hover:shadow-outline border border-' +
-          this.color
-        )
-      else
-        return (
-          'font-extrabold bg-' +
-          this.color +
-          '-300 text-' +
-          this.color +
-          '-800 border-2 border-' +
-          this.color
-        )
+    setBadgeStyle() {
+      let computedClass = ''
+      computedClass =
+        this.color === 'blue' && this.isActive === false
+          ? 'font-medium bg-blue-100 text-blue-800 hover:shadow-outline border border-blue'
+          : this.color === 'blue' && this.isActive
+          ? 'font-extrabold bg-blue-300 text-blue-800 border-2 border-blue'
+          : this.color === 'red' && this.isActive === false
+          ? 'font-medium bg-red-100 text-red-800 hover:shadow-outline border border-red'
+          : this.color === 'red' && this.isActive
+          ? 'font-extrabold bg-red-300 text-red-800 border-2 border-red'
+          : ''
+      return computedClass
     }
   },
   methods: {
